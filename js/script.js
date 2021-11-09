02ac3917caba57576c356eac4b991efd454b7c77
Manuel Guariglia <manuelguariglia@gmail.com>
2021-11-09 15:47:40 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*** 
+ * 1. Estrarre 5 numeri casuali e stamparli a monitor.
+ * 2. Inserire questi dati in un array.
+ * 3. Chiedere all'utente di inserire 5 numeri.
+ * 4. Se il numero inserito dall'utente è nell'array aggiungerlo ad un altro array e incrementare corretti, altrimenti incrementare errori.
+ *  ***/
+
